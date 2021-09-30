e3df20f6a216e12146015f88a83967771572084a
Your Name <you@example.com>
2021-09-30 17:08:46 +0200

    css

--- myApp.js
+++ myApp.js
@@ -7,9 +7,12 @@ console.log("Hello World");
 //2
 //app.get('/',(req,res) => {
 //    res.send('Hello Express');
-//})
+//}
+
 
 //3
+app.use('/public', express.static(__dirname + '/public'));
+//4
 app.get('/',(req,res) => {
     res.sendFile( __dirname +'/views/index.html');
 })
