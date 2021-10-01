bf7de52bb67485ed4153e2e78d72b45fe89250a9
Your Name <you@example.com>
2021-10-01 09:24:21 +0200

    middleware

--- myApp.js
+++ myApp.js
@@ -14,10 +14,20 @@ console.log("Hello World");
 
 //3
 app.use('/public', express.static(__dirname + '/public'));
+
+//6
+let logger = (req, res, next) => {
+    console.log(req.method + ' ' +req.path+' - '+req.ip);
+    next();
+}
+app.use(logger);
+
 //4
 app.get('/',(req,res) => {
     res.sendFile( __dirname +'/views/index.html');
 })
+
+
 
 //5
 app.get('/json',(req,res)=> {
